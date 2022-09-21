cf69b1dc1fc10aa9626cb0c5d8634e5b3e1ee81d
DoudGaya <arbdoule@yahoo.com>
2022-09-21 11:50:28 +0000

    flope

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,7 +5,7 @@ BUZZ = "Buzz"
 
 def fizzbuzz():
     for i in range(1, 101):
-        if (i % 3 == 0) and (i % 5 == 0)
+        if (i % 3 == 0) and (i % 5 == 0):
             print('FizzBuzz')
         elif i % 3 == 0:
             print('Fizz')
