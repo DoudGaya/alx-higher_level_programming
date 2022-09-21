1fcc900adb966ba4ce8a9573683ba840fa9bd629
DoudGaya <arbdoule@yahoo.com>
2022-09-21 12:42:04 +0000

    fope

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,42 +1,33 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * insert_node - inseting a number to a soarted list
- * @head: list head
- * @number: number to store in the new node
- * Return: pointer to the new node
+ * insert_node -  inserts a number into a sorted singly linked list
+ * @head: singly list head
+ * @number: number to insert
+ * Return: the address of the new node, or NULL if it failed
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-        listint_t *runner;
-        listint_t *new;
-
-        runner = *head;
-
-        new = malloc(sizeof(listint_t));
-        if (new == NULL)
-                return (NULL);
-        new->n = number;
-
-        if (*head == NULL || (*head)->n > number)
-        {
-                new->next = *head;
-                *head = new;
-                return (new);
-        }
-
-        while (runner->next != NULL)
-        {
-                if ((runner->next)->n >= number)
-                {
-                        new->next = runner->next;
-                        runner->next = new;
-                        return (new);
-                }
-                runner = runner->next;
-        }
-
-        new->next = NULL;
-        runner->next = new;
-        return (new);
+	listint_t *new;
+	listint_t *current;
+	current = *head;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		while (current->next != NULL)
+		{
+			if (current->next->n > number)
+				break;
+			current = current->next;
+		}
+		new->next = current->next;
+		current->next = new;
+	}
+	return (new);
 }
