11b85ec9b1df74a24dd6347f17a80a77bbc08b9b
DoudGaya <arbdoule@yahoo.com>
2022-09-22 15:00:04 +0000

    dl

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add as addition
+
+if __name__ == "__main__":
+    """Print some of 1 and 2"""
+    from add_0 import add as addition
+
 print("{0} + {1} = {2}".format(1, 2, addition(1, 2)))
