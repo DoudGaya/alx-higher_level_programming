3a720ffda91a00a09548839e9e07f32fac9d4efe
DoudGaya <arbdoule@yahoo.com>
2022-10-10 21:02:57 +0000

    der

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,17 +1,13 @@
+i#!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
 
-
-
-    try:
-
-        for i in range(x):
-
-            print('{}'.format(my_list[i]), end="")
-
-        print('\n')
-
-    except:
-
-        print('Number is not in range')
-
-safe_print_list([1,2,3,4,5], 3)
+    add = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            add += 1
+        except IndexError:
+            break
+    print("")
+    return (add)
