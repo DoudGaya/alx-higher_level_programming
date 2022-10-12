bfc4db42b6b1746400ad78251ce63133138af210
DoudGaya <arbdoule@yahoo.com>
2022-10-12 00:25:33 +0000

    der

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""Creating a sqaure class"""
+
+class Square:
+    """Docs of square class, donesnt really do anything"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Defining a class"""
+
+class Square:
+
+    """Square class"""
+    def __init__(self, size):
+
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
